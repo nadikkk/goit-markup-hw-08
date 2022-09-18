557400dab3a9b273d1eee1c0099924b98954c588
nadikkk <109106413+nadikkk@users.noreply.github.com>
2022-09-18 11:57:48 +0300

    add

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,26 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
